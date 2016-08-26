5680dd2f0fca5111ab100aa7bfd2e5ddb95c51ea
aabdelhak <adel.abdelhak@leansys.fr>
2016-08-26 17:12:57 +0200

    Minor modifs

--- front/app/components/app.component.ts
+++ front/app/components/app.component.ts
@@ -4,6 +4,7 @@ import {WikiBrowseComponent}  from './wiki-browse.component';
 import {WikiViewComponent}    from './wiki-view.component';
 import {WikiEditComponent}    from './wiki-edit.component';
 
+
 @Component({
     selector: 'app',
     template: `
@@ -18,4 +19,29 @@ import {WikiEditComponent}    from './wiki-edit.component';
     </nav>
     <router-outlet></router-outlet>`
 })
+/*
+@Component({
+    selector: 'app',
+    template: `
+    <div class="wrapper">
+         <div id="main-menu">
+             <div id = "title">
+                <h3>The Tool Box</h3>
+            </div>
+            <ul id="left-menu">
+                <li class="item"><a class="item-link active" [routerLink]="['/wiki/browse']">WIKI</a></li>
+                <li class="item"><a class="item-link active" [routerLink]="['/ipbase']">IPBASE</a></li>
+                <li class="item"><a class="item-link active" [routerLink]="['/registry']">REGISTRY</a></li>
+             </ul>
+         </div>
+         <div id="content">
+             <router-outlet></router-outlet>
+         </div>
+         <div id="right-menu">
+                <div id="ss-menu-connexion"><img class="profile" src="app/views/images/profile.png"></i></div>
+                <div id="ss-menu-theme">Thème</div>
+         </div>
+     </div>
+    `
+})*/
 export class AppComponent {}

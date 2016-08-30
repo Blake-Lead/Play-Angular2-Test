98960d1166db8cee9a32cc440299d8c6ed1bd419
aabdelhak <adel.abdelhak@leansys.fr>
2016-08-30 03:10:24 +0200

    refactor(front): enabled prodmode

--- front/app/app.module.ts
+++ front/app/app.module.ts
@@ -1,22 +1,24 @@
-import {NgModule}               from '@angular/core';
-import {BrowserModule}          from '@angular/platform-browser';
-import {FormsModule}            from '@angular/forms';
-import {HttpModule}             from '@angular/http';
+import {NgModule, enableProdMode} from '@angular/core';
+import {BrowserModule}            from '@angular/platform-browser';
+import {FormsModule}              from '@angular/forms';
+import {HttpModule}               from '@angular/http';
 
-import {routing}                from './app.routes';
+import {routing}                  from './app.routes';
 
-import {AppComponent}           from './components/app.component';
-import {HomeComponent}          from './components/home.component';
-import {WikiBrowseComponent}    from './components/wiki-browse.component';
-import {WikiViewComponent}      from './components/wiki-view.component';
-import {WikiEditComponent}      from './components/wiki-edit.component';
-import {IpBaseComponent}        from './components/ipbase.component';
-import {RegistryComponent}      from './components/registry.component';
+import {AppComponent}             from './components/app.component';
+import {HomeComponent}            from './components/home.component';
+import {WikiBrowseComponent}      from './components/wiki-browse.component';
+import {WikiViewComponent}        from './components/wiki-view.component';
+import {WikiEditComponent}        from './components/wiki-edit.component';
+import {IpBaseComponent}          from './components/ipbase.component';
+import {RegistryComponent}        from './components/registry.component';
 
-import {WikiService}            from './services/wiki.service';
-import {MarkedService}          from './services/marked.service';
-import {IpBaseService}          from './services/ipbase.service';
-import { MODAL_DIRECTIVES }     from 'ng2-bs3-modal/ng2-bs3-modal';
+import {WikiService}              from './services/wiki.service';
+import {MarkedService}            from './services/marked.service';
+import {IpBaseService}            from './services/ipbase.service';
+import { MODAL_DIRECTIVES }       from 'ng2-bs3-modal/ng2-bs3-modal';
+
+enableProdMode();
 
 @NgModule({
     imports: [
